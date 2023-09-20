ebdfe6d7d8e975d4c9d8d0cf836b8de4969f8577
Shallon Mokeira Said <shallonmokeira@gmail.com>
2023-09-20 14:52:53 -0400

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    def match(self, word):
+        anagrams = []
+        for w in word:
+            if w != self.word and sorted(w) == sorted(self.word):
+                anagrams.append(w)
+        return anagrams        
